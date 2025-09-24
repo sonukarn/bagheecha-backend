7a423b2c3a3a1d9026eb77f62e2002d58be73b48
sonukarn <s.sonukarn.1997@gmail.com>
2025-09-24 16:53:03 +0530

    using mysqllite db

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: env('PUBLIC_URL', 'https://your-strapi-url.com'), // Replace later
+  url: env('PUBLIC_URL', 'https://your-strapi-url.com'), // Replace later
   app: {
     keys: env.array('APP_KEYS'),
   },
